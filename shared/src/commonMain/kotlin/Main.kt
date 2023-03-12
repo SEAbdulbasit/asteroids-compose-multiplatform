9159024036b9db0a08e2a8e08e8b681a05a98bc4
Abdul Basit <abdulbasit5361234@gmail.com>
2023-03-12 09:56:28 +0500

    pointerMove removed.

--- shared/src/commonMain/kotlin/Main.kt
+++ shared/src/commonMain/kotlin/Main.kt
@@ -2,6 +2,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,11 +21,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import vector.onMove
 
 @Composable
 internal fun GameView() {
@@ -56,12 +57,14 @@ internal fun GameView() {
                 BorderStroke(1.dp, Color.White), shape = RoundedCornerShape(8.dp)
             ).background(Color(0, 0, 30)).clip(RoundedCornerShape(8.dp))
         ) {
-            Box(modifier = Modifier.fillMaxWidth().fillMaxHeight().clickable() {
+            Box(modifier = Modifier.fillMaxWidth().fillMaxHeight().clickable {
                 game.ship.fire(game)
-            }.onMove { offset ->
-                with(density) {
-                    game.targetLocation = DpOffset(offset.x.toDp(), offset.y.toDp())
-                }
+            }.pointerInput(Unit) {
+                detectDragGestures(onDrag = { change, _ ->
+                    game.targetLocation =
+                        DpOffset(change.position.x.toDp(), change.position.y.toDp())
+
+                })
             }.onSizeChanged {
                 with(density) {
                     game.width = it.width.toDp()
--- shared/src/commonMain/kotlin/vector/PointerMove.kt
+++
@@ -1,9 +0,0 @@
-package vector
-
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-
-/**
- * Created by abdulbasit on 11/03/2023.
- */
-expect fun Modifier.onMove(onOffsetChanged: (position: Offset) -> Unit): Modifier
