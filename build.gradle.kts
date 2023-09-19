19fd051d37fe3ae09a65b8b369939e7b638d9ab3
Abdul Basit <abdulbasit5361234@gmail.com>
2023-09-19 21:55:21 +0500

    Production distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application").version("8.1.0-alpha05").apply(false)
-    id("com.android.library").version("8.1.0-alpha05").apply(false)
+    id("com.android.application").version("8.1.0-alpha08").apply(false)
+    id("com.android.library").version("8.1.0-alpha08").apply(false)
     id("org.jetbrains.compose").version("1.3.0") apply false
     kotlin("android").version("1.8.0").apply(false)
     kotlin("multiplatform").version("1.8.0").apply(false)
